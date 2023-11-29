9f2f4a20e61fb7d73c8b45778c21bab6d9abe60a
wahidh007 <hamdi.wahid2@gmail.com>
2023-11-29 11:01:32 +0100

    Update vagrant_repo.sh

--- vagrant_repo.sh
+++ vagrant_repo.sh
@@ -7,4 +7,6 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-sudo apt update && sudo apt install vagrant
+# Install Vagrant version=2.3.4 on Ubuntu 22.04.3 LTS to prevent the compatibility problem with ansible
+sudo apt update && sudo apt install vagrant=2.3.4
+sudo apt-mark hold vagrant=2.3.4
